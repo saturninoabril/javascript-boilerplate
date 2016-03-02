e7740ec23c0a341bc4b9fd461a76ad6b0cde47d6
Brikou Carré <brikou@gmail.com>
2016-03-02 17:46:15 +0100

    Revert out of scope changes, update README

--- config/default.js
+++ config/default.js
@@ -1,12 +1,12 @@
 var apiPort = process.env.NODE_PORT || 3000; // eslint-disable-line no-var
-var apiUrl = `http://localhost:${apiPort}`; // eslint-disable-line no-var
+var apiUrl = 'http://localhost:' + apiPort; // eslint-disable-line no-var
 var frontendUrl = 'http://localhost:8080'; // eslint-disable-line no-var
 
 module.exports = {
     appName: 'New App',
     apps: {
         admin: {
-            api_url: `${apiUrl}/admin/`,
+            api_url: apiUrl + '/admin/',
         },
         api: {
             allowOrigin: [frontendUrl],
